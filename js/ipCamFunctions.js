3f5a453fc99402c1dc90e1e6c8c20dbf47dfb241
DevBishop <matteo.dp@me.com>
2016-04-10 00:32:26 +0200

    new:xml Parser js recupero credenziali e altro

--- js/ipCamFunctions.js
+++ js/ipCamFunctions.js
@@ -1,105 +1,108 @@
 
 var camWebApi = {
-	
-	 webApiUrl : function(){
-		 return "http://192.168.1.238:88/cgi-bin/CGIProxy.fcgi?";
-	 },
-     
-	 //0:admin, 1:operator, 2:viewer
-	 userCredentialsByType : function(typeOfUser){
-		 switch(typeOfUser){
-			case 1:
-				return {user : "matteoo", password : "matteoo"}; //operator
-			case 2:
-				return {user : "matteov", password : "matteov"}; //viewer
-			default:
-				return {};
-		 }
-	 },
-     
-	 requiredActionFromKeyCode : function(e){
-         debugger;
-			switch(e.keyCode) {
-			case 37:
-				return "Left"
-			case 38: 
-				return "Up"
-			case 39: 
-				return "Right"
-			case 40: 
-				return "Down"
-			default: 
+    
+    webApiUrl : function(){
+        return this.xmlParser('ipCamUrl', 0).url + "/cgi-bin/CGIProxy.fcgi?";
+    },
+    
+    xmlFilePath : function(){
+        return '/../config.xml';
+    },
+    //0:admin, 1:operator, 2:viewer
+    userCredentialsByType : function(typeOfUser){
+        return this.xmlParser('credentials', typeOfUser);
+    },
+    
+    requiredActionFromKeyCode : function(e){
+            switch(e.keyCode) {
+            case 37:
+                return "Left"
+            case 38: 
+                return "Up"
+            case 39: 
+                return "Right"
+            case 40: 
+                return "Down"
+            default: 
             return; 
-		 }
-	 },
-	 
-	 buildWebApiGetFromGenericRequest : function(action, typeOfUser){
-		 var url = "";
-		 switch(action){
-			 case "snapPicture2":
-			 	url = this.webApiUrl() + "cmd=" + 
-			 	action + "&usr=" + 
-				this.userCredentialsByType(typeOfUser).user + "&pwd=" +
-		 		this.userCredentialsByType(typeOfUser).password + "&"; 
-				return url;
-		 }
-	 },
-	 
-	 buildWebApiGetFromKeyCodeAction : function(action, e){
-		var url = "";
-		 switch (action) {
-			 case "Move":
-			 	url = this.webApiUrl() + "cmd=ptzMove" + 
-				this.requiredActionFromKeyCode(e) + "&usr=" + 
-				this.userCredentialsByType(1).user + "&pwd=" +
-		 		this.userCredentialsByType(1).password; 
-				$.getJSON(url, function(data){});
-				 break;
-			 case "Stop":
-				url = this.webApiUrl() + "cmd=ptzStopRun" + 
-				this.requiredActionFromKeyCode(e) + "&usr=" + 
-				this.userCredentialsByType(1).user + "&pwd=" +
-		 		this.userCredentialsByType(1).password;
-				$.getJSON(url, function(data){});
-				break;
-		 }
-	 },
-     buildWebApiStopRunUrl : function(){
-                this.webApiUrl() + "cmd=ptzStopRun" + 
-				this.requiredActionFromKeyCode(e) + "&usr=" + 
-				this.userCredentialsByType(1).user + "&pwd=" +
-		 		this.userCredentialsByType(1).password;
-          
-				$.getJSON(url, function(data){});
-     },
-     
-     xmlParser : function(xmlText){
-         debugger;
-         if (window.DOMParser)
-            {
-                parser=new DOMParser();
-                xmlDoc=parser.parseFromString(xmlText,"text/xml");
+        }
+    },
+    
+    buildWebApiGetFromGenericRequest : function(action, typeOfUser){
+        var url = "";
+        switch(action){
+            case "snapPicture2":
+                url = this.webApiUrl() + "cmd=" + 
+                action + "&usr=" + 
+                this.userCredentialsByType(typeOfUser).user + "&pwd=" +
+                this.userCredentialsByType(typeOfUser).password + "&"; 
+                return url;
+        }
+    },
+    
+    buildWebApiGetFromKeyCodeAction : function(action, e){
+        var url = "";
+        switch (action) {
+            case "Move":
+                url = this.webApiUrl() + "cmd=ptzMove" + 
+                this.requiredActionFromKeyCode(e) + "&usr=" + 
+                this.userCredentialsByType(1).user + "&pwd=" +
+                this.userCredentialsByType(1).password; 
+                $.getJSON(url, function(data){});
+                break;
+            case "Stop":
+                url = this.webApiUrl() + "cmd=ptzStopRun" + 
+                this.requiredActionFromKeyCode(e) + "&usr=" + 
+                this.userCredentialsByType(1).user + "&pwd=" +
+                this.userCredentialsByType(1).password;
+                $.getJSON(url, function(data){});
+                break;
+        }
+    },
+    buildWebApiStopRunUrl : function(){
+        this.webApiUrl() + "cmd=ptzStopRun" + 
+        this.requiredActionFromKeyCode(e) + "&usr=" + 
+        this.userCredentialsByType(1).user + "&pwd=" +
+        this.userCredentialsByType(1).password;
+        $.getJSON(url, function(data){});
+    },
+    
+    xmlFileGetter : function(xmlFile){
+        var xmlDoc;
+        if(typeof window.DOMParser != "undefined") {
+            xmlhttp=new XMLHttpRequest();
+            xmlhttp.open("GET",xmlFile,false);
+            if (xmlhttp.overrideMimeType){
+                xmlhttp.overrideMimeType('text/xml');
             }
-            else // Internet Explorer
-            {
-                xmlDoc=new ActiveXObject("Microsoft.XMLDOM");
-                xmlDoc.async=false;
-                xmlDoc.loadXML(txt);
-            }
-            
-            var xmlObj = {
-          
-                user0 = xmlDoc.getElementsByTagName("user0")[0].childNodes[0].nodeValue,
-                password0 = xmlDoc.getElementsByTagName("password0")[0].childNodes[0].nodeValue,
-                user1 = xmlDoc.getElementsByTagName("user1")[0].childNodes[0].nodeValue,
-                password1 = xmlDoc.getElementsByTagName("password1")[0].childNodes[0].nodeValue,
-                user2 = xmlDoc.getElementsByTagName("user2")[0].childNodes[0].nodeValue,
-                password2 = xmlDoc.getElementsByTagName("password2")[0].childNodes[0].nodeValue,
-            }
-            
-            return xmlObj;
-     }
-	 
-	 
-	
+            xmlhttp.send();
+            xmlDoc=xmlhttp.responseXML;
+        }
+        else{
+            xmlDoc = new ActiveXObject("Microsoft.XMLDOM");
+            xmlDoc.async="false";
+            xmlDoc.load(xmlFile);
+        }
+        return xmlDoc;
+    },
+    
+    xmlParser : function(typeOfNode, typeOfUser){
+        var xmlDoc = this.xmlFileGetter(this.xmlFilePath());
+        var pNode=xmlDoc.getElementsByTagName(typeOfNode);
+        
+        switch(typeOfNode){
+            case "credentials":
+                return credentialsObj = {
+                    user : pNode[0].getElementsByTagName("user" + typeOfUser)[0].childNodes[0].nodeValue,
+                    password : pNode[0].getElementsByTagName("password" + typeOfUser)[0].childNodes[0].nodeValue,
+                }
+            case "ipCamUrl":
+                return urlObj = {
+                    url : pNode[0].getElementsByTagName("url")[0].childNodes[0].nodeValue
+                } 
+            default:
+                return "";
+        }   
+    }
+    
 }
